73ede914dc19b47872d502fe41ebb24d11efe711
alpererdogan8 <alpererdogan8@gmail.com>
2026-08-16 22:12:43 +0300

    chore(hypr): remove hyprland config directory from main
    Full config preserved in the 'hyprland' branch.
    Restore with: git checkout hyprland -- hypr/

--- hypr/hyprland.lua
+++
@@ -1,9 +0,0 @@
--- Hyprland Lua Config
--- https://wiki.hypr.land/Configuring/Start/
---require("monitors")
-require("modules.monitors")
-require("modules.env")
-require("modules.autostart")
-require("modules.config")
-require("modules.rules")
-require("modules.keybinds")
